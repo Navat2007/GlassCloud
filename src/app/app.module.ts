40217f59a9aecbcc1e6239432e8d515e3f15edda
Vladimir Minikh <minih@omnicomm.ru>
2018-05-10 12:15:19 +0300

    add nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const appRoutes: Routes = [
   { path: 'table', component: TableComponent }
@@ -13,14 +15,16 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes
     ),
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
