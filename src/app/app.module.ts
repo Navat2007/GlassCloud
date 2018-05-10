0db7a0ad88ea18fa338fc81f1bbde7acfa21dd3d
Vladimir Minikh <minih@omnicomm.ru>
2018-05-10 10:22:17 +0300

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
@@ -18,7 +19,8 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes
-    )
+    ),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
